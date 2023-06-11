dc10e8f4146a3c70b168aa0718c5f7e8ba737723
Kasek95 <pawel.kasek62@gmail.com>
2023-06-12 00:41:40 +0200

    add Login Page, Register Page

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './login.scss';
 const Login: React.FC = () => {
-	return ( <>
-    </>
-    )
+	return (
+		<section className='login'>
+			<div className='container loginContainer'>
+				<h2>Zaloguj Się</h2>
+				<div className='decoration'></div>
+				<div className='formContainer'>
+					<form>
+						<div className='inputContainer'>
+							<label htmlFor='Email'>Email</label>
+							<input id='Email'></input>
+						</div>
+						<div className='inputContainer'>
+							<label htmlFor='Password'>Hasło</label>
+							<input id='Password'></input>
+						</div>
+					</form>
+					<div className='btnContainer'>
+						<button>
+							<Link to='/Register'>Załóż Konto</Link>
+						</button>
+						<button>
+							<Link to='/OddajRzeczy'>Zaloguj Się</Link>
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Login;
--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './register.scss'
 const Register: React.FC = () => {
-	return <></>;
+	return (
+		<section className='Register'>
+			<div className='container registerContainer'>
+				<h2>Załóż konto</h2>
+				<div className='decoration'></div>
+				<div className='formContainer'>
+					<form className='formRegister'>
+						<div className='inputContainer'>
+							<label htmlFor='Email'>Email</label>
+							<input id='Email'></input>
+						</div>
+						<div className='inputContainer'>
+							<label htmlFor='Password'>Hasło</label>
+							<input id='Password'></input>
+						</div>
+						<div className='inputContainer'>
+							<label htmlFor='Password2'>Powtórz hasło</label>
+							<input id='Password2'></input>
+						</div>
+					</form>
+					<div className='btnContainer'>
+						<button>
+							<Link to='/Login'>Zaloguj się</Link>
+						</button>
+						<button>
+							<Link to='/'>Załóż konto</Link>
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 };
 
 export default Register;
