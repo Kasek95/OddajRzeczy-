f845e7f714195fef7522fb7db70f0dbfe459bd79
Kasek95 <pawel.kasek62@gmail.com>
2023-06-12 03:00:57 +0200

    start making givawey info

---
+++ src/components/homePage/infoAbouGivaweyItem/InfoAboutGivawaey.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './infoAboutGivaweyItem.scss';
+
+const InfoAboutGivaweyItem: React.FC = () => {
+    return (
+        <section className="InfoAboutGivawey">
+            <article className="container info">
+                
+            </article>
+        </section>
+    )
+}
+
+export default InfoAboutGivaweyItem;
--- src/components/homePage/main/Main.tsx
+++ src/components/homePage/main/Main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import WelcomeSection from '../welcomSection/WelcomSection';
+import InfoAboutGivaweyItem from '../infoAbouGivaweyItem/InfoAboutGivawaey';
 import "./main.scss"
 const Main:React.FC = () => {
 	return (
         <>
          <main>
          <WelcomeSection/>
+         <InfoAboutGivaweyItem/>
          </main>
          
         </>
