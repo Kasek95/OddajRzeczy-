a35adc6a3f04e0e2e247545c458f4c9db5ef1fd8
Kasek95 <pawel.kasek62@gmail.com>
2023-06-19 02:14:14 +0200

    add pagination

--- src/components/homePage/whoWeHelp/WhoWeHelp.tsx
+++ src/components/homePage/whoWeHelp/WhoWeHelp.tsx
@@ -1,7 +1,32 @@
 import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import FundationData from './fundationData';
 import './whoWeHelp.scss';
 
 const WhoWeHelp: React.FC = () => {
+	
+const [fundation, setFundation] = useState(true);
+	const [organization, setOrganization] = useState(false);
+	const [local, setLocal] = useState(false);
+	const [fundadtionPageNumber, setFundadionPageNumber] = useState(0);
+	const fundationPerPage = 3;
+	const fundationPageVisited = fundadtionPageNumber * fundationPerPage;
+	const fundationCount = Math.ceil(FundationData.length / fundationPerPage);
+	const displayFundationData = FundationData
+		.slice(fundationPageVisited, fundationPageVisited + fundationPerPage)
+		.map((fundation, idx) => (
+			<div key={idx} className='singielFundation'>
+				<div className='infoContainer'>
+					<span>Fundacja {fundation.fundation}</span>
+					<span>Cel i misja {fundation.for}</span>
+				</div>
+				<span>{fundation.item}</span>
+			</div>
+		));
+	const pageChange = ({ selected }: any) => {
+		setFundadionPageNumber(selected);
+	};
+	console.log(FundationData);
 	return (
 		<section className='whoWeHelp'>
 			<article className='whoWeHelpContainer container'>
@@ -9,11 +34,13 @@ const WhoWeHelp: React.FC = () => {
 					<h2>Komu Pomagamy?</h2>
 					<div className='decoration'></div>
 					<div className='organizationBox'>
-						<span>Fundacjom</span>
-						<span>
+						<span className={fundation ? 'span active' : 'span'}>
+							Fundacjom
+						</span>
+						<span className={organization ? 'span active' : 'span'}>
 							Organizacjom<br></br>pozarządowym
 						</span>
-						<span>
+						<span className={local ? 'span active' : 'span'}>
 							Lokalnym<br></br>zbiórkom
 						</span>
 					</div>
@@ -22,6 +49,18 @@ const WhoWeHelp: React.FC = () => {
 						współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i
 						czego potrzebują.
 					</p>
+					<section className='paginationSection'>
+						<article className={fundation ? 'singielList show' : ''}>
+							{displayFundationData}
+							<ReactPaginate
+								pageCount={fundationCount}
+								onPageChange={pageChange}
+								containerClassName='paginationBtns'
+								disabledClassName='paginationDisabled'
+								activeClassName='paginationActive'
+							/>
+						</article>
+					</section>
 				</div>
 			</article>
 		</section>
---
+++ src/components/homePage/whoWeHelp/fundationData.tsx
@@ -0,0 +1,55 @@
+interface Data {
+	fundation: string;
+	for: string;
+	item: string;
+}
+
+const FundationData: Data[] = [
+	{
+		fundation: 'Dba o zdrowie',
+		for: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+		item: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki',
+	},
+    {
+		fundation: 'Dla dzieci',
+		for: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
+		item: 'ubrania, meble, zabawki',
+	},
+    {
+		fundation: 'Bez domu',
+		for: 'Pomoc dla osób nie posiadających miejsca zamieszkania.',
+		item: 'ubrania, jedzenie, ciepłe koce',
+	},
+    {
+		fundation: 'Dba o zdrowie',
+		for: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+		item: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki',
+	},
+    {
+		fundation: 'Dba o zdrowie',
+		for: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+		item: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki',
+	},
+    {
+		fundation: 'Dba o zdrowie',
+		for: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+		item: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki',
+	},
+    {
+		fundation: 'Dba o zdrowie',
+		for: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+		item: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki',
+	},
+    {
+		fundation: 'Dba o zdrowie',
+		for: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+		item: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki',
+	},
+    {
+		fundation: 'Dba o zdrowie',
+		for: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+		item: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki',
+	}
+];
+
+export default FundationData;
